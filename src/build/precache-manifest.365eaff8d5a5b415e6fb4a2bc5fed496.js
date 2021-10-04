7dc3ecaac01ebfe091404530ed9b168aa2c84b8b
sdkdeepa <deepa94086@gmail.com>
2021-10-04 01:46:28 -0700

    first backend commit

---
+++ src/build/precache-manifest.365eaff8d5a5b415e6fb4a2bc5fed496.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51037199c29c52698b00",
+    "url": "/static/css/main.052dea8a.chunk.css"
+  },
+  {
+    "revision": "51037199c29c52698b00",
+    "url": "/static/js/main.785aa6a7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a198b91a2dbac503ae59",
+    "url": "/static/js/2.bd616815.chunk.js"
+  },
+  {
+    "revision": "6dd28e1b4d99e0d2d4dd6718defb1dd1",
+    "url": "/index.html"
+  }
+];
